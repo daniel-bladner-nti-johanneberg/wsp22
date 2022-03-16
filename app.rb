791a6128ef41771e28de627ca87a6c5eca27e47a
daniel-bladner-nti-johanneberg <74768197+daniel-bladner-nti-johanneberg@users.noreply.github.com>
2022-03-16 16:40:50 +0100

    Påbörjad post

--- app.rb
+++ app.rb
@@ -3,13 +3,6 @@ require 'slim'
 require 'sqlite3'
 enable :sessions
 
-def connect_to_db(path)
-    db = SQLite3::Database.new(path)
-    db.results_as_hash = true
-    return db
-end
-
-
 get('/') do      
     slim(:start)
 end
@@ -18,14 +11,25 @@ end
 get('/affar') do
     db = SQLite3::Database.new("db/database.db")
     db.results_as_hash = true
-    result = db.execute("SELECT * FROM Book")
+    result = db.execute("SELECT * FROM book")
     p result
-    slim(:"affar/index",locals:{book:result})
-  
-  
-  
+    slim(:"affar/index",locals:{books:result})
   end
 
 
 
+get('/affar/new') do 
+  slim(:"affar/new")
+end
+  
+post('/affar/new') do
+    book_name = params[:Name]
+    book_id = params[:id].to_i
+    book_price = params[:Price].to_f
+    book_img = params[:img]
+    book_stock = params[:Stock].to_i
+    db = SQLite3::Database.new("db/database.db")
+    db.execute("INSERT INTO book (Name, id, Price, Stock, img ) VALUES (?,?,?,?,?)",book_name,book_id,book_price,book_stock,book_img)
+    redirect('/affar/new')
+end
 
